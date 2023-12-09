5071698bb89cf692e2c94f35e0ab07761e0a7764
mehreenprogrammer <152857879+mehreenprogrammer@users.noreply.github.com>
2023-12-09 03:56:51 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,58 @@
+let powder = "lal mirch powder";
+powder ="haldi";
+console.log("hello");
+console.log(powder);
+let fulName:string ="Hamzah";
+fulName ="okasha"
+fulName ="ali"
+fulName ="hamzah"
+console.log(fulName);
+let fulNam ="hamzah syed";
+let age =22
+let bulb =true;
+let bulb2 =false;
+fulName;
+console.log(fulNam);
+fulNam ="hamzah";
+let firstName ="hamzah";
+let lastName ="syed";
+console.log(firstName +"" + lastName);
+console.log(`my name is ${firstName } ${lastName}`);
+let qty = 8;
+console.log(qty +10);
+console.log(qty-1);
+console.log(qty*12000);
+console.log(qty*12000);
+console.log(qty/3)
+let str1 ="1";
+let str2 ="2";
+console.log(str1+str2);
+let n1 =2;
+let n2 =3 ;
+console.log(n1**n2);
+console.log(qty%3)
+
+let Age =22;
+Age+=10;
+age-=10;
+console.log(age);
+let n=5;
+console.log(n);5
+n+=5;
+console.log(n);10
+n-=5;
+console.log(n);5
+let aage=22;
+console.log(age==22);
+console.log(age===22);
+console.log(age!=22);
+console.log(age>10);
+console.log(age<=22);
+let N=5;
+console.log(n==5);true
+console.log(n===5);true
+console.log(n!=5);false
+console .log(n>8);false
+console.log(n<5);false
+console.log(n>=8);false
+console .log(n<8);true
